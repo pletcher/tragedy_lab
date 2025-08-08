b0393c0d601d6fb0c20f80f5cad44af615eb1f47
Charles Pletcher <cpletcher2@gmail.com>
2025-08-08 16:43:12 -0400

    Collect speeches using data from DICES

--- main.py
+++ main.py
@@ -2,19 +2,7 @@ import json
 
 from src import write_homer_df, write_tragedy_df, write_corpus
 from src.lemmatize import lemmatize
-from src.homeric_speeches import DICESClient
+from src.homeric_speeches import DICESClient, write_speeches_as_docs
 
 if __name__ == "__main__":
-    iliad_client = DICESClient("Iliad")
-
-    iliad_speeches = iliad_client.get_speeches()
-
-    with open("homeric_speeches/iliad_speeches.json", "w") as f:
-        f.write(json.dumps(iliad_speeches, indent=4))
-
-    odyssey_client = DICESClient("Odyssey")
-
-    odyssey_speeches = odyssey_client.get_speeches()
-
-    with open("homeric_speeches/odyssey_speeches.json", "w") as f:
-        f.write(json.dumps(odyssey_speeches, indent=4))
+    write_speeches_as_docs()
--- src/homeric_speeches.py
+++ src/homeric_speeches.py
@@ -1,7 +1,9 @@
+import json
 import time
 
 from typing import Literal
 
+import polars as pl
 import requests
 
 
@@ -34,3 +36,65 @@ class DICESClient:
             time.sleep(0.3)
 
         return self.speeches
+
+
+def get_lines_from_dataframe(
+    df: pl.DataFrame, title: str, lines: list[int]
+):
+    rows = (
+        df.filter(
+            pl.col("title").eq(title)
+            & pl.col("book_n").is_in([l[0] for l in lines])
+            & pl.col("n").ge(lines[0][1])
+            & pl.col("n").le(lines[1][1])
+        )
+        .select(pl.col("text"))
+    ).rows()
+
+    return [r[0] for r in rows]
+
+
+def get_speeches_by_title_and_speaker(
+    df: pl.DataFrame, title: Literal["Iliad", "Odyssey"]
+):
+    with open(f"./dices_speeches/{title.lower()}_speeches.json") as f:
+        speeches = json.load(f)
+
+    speeches_by_speaker = {}
+
+    for speech in speeches:
+        speaker_id = "-".join([f"{s["name"]}@{s["id"]}" for s in speech["spkr"]])
+
+        if speaker_id not in speeches_by_speaker:
+            speeches_by_speaker[speaker_id] = []
+
+        first_line = [int(n) for n in speech["l_fi"].split(".")]
+        last_line = [int(n) for n in speech["l_la"].split(".")]
+
+        lines = get_lines_from_dataframe(df, title, [first_line, last_line])
+
+        speeches_by_speaker[speaker_id] += lines
+
+    return speeches_by_speaker
+
+
+def write_speeches_as_docs():
+    homer_df = pl.read_parquet("./homer.parquet").cast(
+        {"book_n": pl.UInt8, "n": pl.UInt16}
+    )
+
+    iliad = get_speeches_by_title_and_speaker(homer_df, "Iliad")
+
+    for speaker, lines in iliad.items():
+    	with open(f"./homeric_corpus/Iliad_{speaker}.txt", "w") as f:
+            f.write("\n".join(lines))
+
+    odyssey = get_speeches_by_title_and_speaker(homer_df, "Odyssey")
+
+    for speaker, lines in odyssey.items():
+        with open(f"./homeric_corpus/Odyssey_{speaker}.txt", "w") as f:
+            f.write("\n".join(lines))
+
+def speeches_to_conllu():
+	pass
+
